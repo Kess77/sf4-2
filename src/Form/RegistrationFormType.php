58420e4a756a40a4feb5a33ea8841e698ce65852
kess <enikabe@yahoo.fr>
2020-06-30 14:59:57 +0200

    INSTALLE  REGISTERFORM
    Installation de formulaire d'inscription, ajout dans le dossier security

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(['message'=>'Veuillez indiquer une adresse email']),
+                    new Email(['message'=>'Adresse email incorrecte'])
+                ]
+            ])
+            // ceci contient le hash et faire confirmer les mots de passes
+            ->add('plainPassword', RepeatedType::class, [
+                'type'=>PasswordType::class, // choix de confirmation
+                'invalid_message'=>'les mots de passe ne correspondent pas',
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mots de passe',
+                    ]),
+                    // les contraintes de la longueurs du mots de passe
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Votre mot de passe doit faire au moins {{ limit }} caractères ',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
